1c793af2162a9b5f19cff0b9388e26143ba54798
Kevin Papst <kpapst@gmx.net>
2023-09-18 18:13:36 +0200

    code styles

--- Timesheet/Calculator/DeductionTimeCalculator.php
+++ Timesheet/Calculator/DeductionTimeCalculator.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the DeductionTimeBundle for Kimai.
+ * This file is part of the "DeductionTimeBundle" for Kimai.
  * All rights reserved by Kevin Papst (www.keleo.de).
  *
  * For the full copyright and license information, please view the LICENSE
