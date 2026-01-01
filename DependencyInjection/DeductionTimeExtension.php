83c5e750f4a009bc9f6bcf966a8882f0b2b2d868
Kevin Papst <kevin@kevinpapst.de>
2026-01-01 16:41:12 +0100

    code style

--- DependencyInjection/DeductionTimeExtension.php
+++ DependencyInjection/DeductionTimeExtension.php
@@ -12,8 +12,8 @@ namespace KimaiPlugin\DeductionTimeBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class DeductionTimeExtension extends Extension
 {
